ffae18f6abb95b79f05c933d06507e1bd2791958
matheuswisch <wisch.pg@outlook.com>
2023-05-04 15:53:19 -0300

    Task: Finishing the navbar and the navbar responsiviness

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
 const  App = () =>  {
 
   return (
     <div>
-      3d dev porfolio
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-patter bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <Contact />
+          <StarsCanvas />
+        </div>
+        <div className='relatize z-0'>
+          
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
