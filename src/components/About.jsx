b66751692a9df846105789da957285789126a585
matheuswisch <wisch.pg@outlook.com>
2023-05-05 08:43:24 -0300

    Task: Ending section about and adding a sectionwrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 
 const ServiceCard =({ index, title, icon}) => {
@@ -14,7 +15,19 @@ const ServiceCard =({ index, title, icon}) => {
           variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
           className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
         >
-
+            <div 
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450
+            }} 
+            className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
+            flex justify-evenly items-center flex-col'
+            >
+              <img src={icon} alt={title} 
+              className='w-16 h-16 object-contain'/>
+              <h3 className='text-white text-[20px] font-bold text-center' >{title}</h3>  
+            </div>
         </motion.div>
     </Tilt>
   )
@@ -45,4 +58,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
