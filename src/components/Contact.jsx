d01c0ae6fc8351bb215c69292a7d5025844b03f6
Matheus Wisch <wisch.pg@outlook.com>
2023-05-24 10:09:17 -0300

    Task: retirando projetos e testemunhas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {}
+  
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+      <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+          <motion.div
+            variants={slideIn('left', "tween", 0.2, 1)}
+            className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+          >
+            <p className={styles.sectionSubText}>
+              Fale comigo
+            </p>
+            <h3 className={styles.sectionHeadText}>
+              Contato.
+            </h3>
+
+            <form></form>
+          </motion.div>
+      </div>  
   )
+
 }
 
 export default Contact
