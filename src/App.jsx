b3267942f31afe1ffa6913eaede34028328b4120
Wisch096 <wisch.pg@outlook.com>
2024-01-10 09:23:28 -0300

    finish ajustes.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        <Tech />
+        {/* <Tech /> */}
         {/* <Works /> */}
         {/* <Feedbacks />  */}
         <div className='relative z-0'>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,20 +45,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        Comecei minha carrreira como Desenvolvedor na TopSoft Informática como desenvolvedor full stack. Atualmente, sou Desenvolvedor Web na Captar Agrobusiness.
-
-        Competências técnicas:
-
-          Python
-        - Django
-        - Java
-        - SpringBoot
-        - JavaScript/TypeScript
-        - ReactJS
-        - NextJS
-        - PostgreSQL
-        - SQLServer
-        - Docker
+        Comecei minha carrreira como Desenvolvedor na TopSoft Informática como desenvolvedor full stack. Atualmente, sou Desenvolvedor FullStack na Conceito Tecnologia.
       </motion.p>
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
